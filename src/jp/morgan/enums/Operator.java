1ccd2a0f0bfb05080345db3da50d2087a4b5e774
qualtardemix@gmail.com <qualtardemix@gmail.com>
2017-10-11 14:32:18 +0530

    Second Technical test : Application for message processing.

---
+++ src/jp/morgan/enums/Operator.java
@@ -0,0 +1,16 @@
+package jp.morgan.enums;
+
+public enum Operator {
+	add, subtract, multiply;
+
+	public Integer calculate(Integer num1, Integer num2) {
+		if (this.equals(Operator.add)) {
+			return num1 + num2;
+		} else if (this.equals(Operator.subtract)) {
+			return num1 - num2;
+		} else if (this.equals(Operator.multiply)) {
+			return num1 * num2;
+		}
+		return null;
+	}
+}
